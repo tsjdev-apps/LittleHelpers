ee47ff8ecdd9f55da68ebceb0162f67d6743f0aa
Sebastian Jensen <apps@tsjdev-apps.de>
2017-11-18 19:04:52 +0100

    - add enum helper tests

---
+++ LittleHelpers/LittleHelpers.Tests/Data/GenderWithDefault.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace LittleHelpers.Tests.Data
+{
+
+
+    public enum GenderWithDefault
+    {
+        Female,
+        [DefaultValue(Male)]
+        Male
+    }
+}
---
+++ LittleHelpers/LittleHelpers.Tests/Helpers/EnumHelpersTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using LittleHelpers.Helpers;
+using LittleHelpers.Tests.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LittleHelpers.Tests.Helpers
+{
+    [TestClass]
+    public class EnumHelpersTest
+    {
+        [TestMethod]
+        public void EnumHelper_GetEnumDefault_ReturnsAsDefaultMarkedValue()
+        {
+            // act
+            var enumValue = EnumHelper.GetEnumDefault<GenderWithDefault>();
+
+            // assert
+            Assert.AreEqual(GenderWithDefault.Male, enumValue);
+        }
+
+        [TestMethod]
+        public void EnumHelper_GetEnumDefault_ReturnsFirstValue()
+        {
+            // act
+            var enumValue = EnumHelper.GetEnumDefault<Gender>();
+
+            // assert
+            Assert.AreEqual(Gender.Female, enumValue);
+        }
+
+        [TestMethod]
+        public void EnumHelper_GetEnumAsEnumarable_ReturnsEnumerableOfEnumValues()
+        {
+            // act
+            var enumValues = EnumHelper.GetEnumAsEnumarable<Gender>().ToList();
+
+            // assert
+            Assert.AreEqual(2, enumValues.Count());
+            Assert.AreEqual(Gender.Female, enumValues.First());
+        }
+
+    }
+}
