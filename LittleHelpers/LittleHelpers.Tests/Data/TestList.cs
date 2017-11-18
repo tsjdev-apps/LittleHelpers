d35cf13a332c4e65f3c565177430eb6f6fc3d1f6
Sebastian Jensen <apps@tsjdev-apps.de>
2017-11-18 19:19:16 +0100

    - add enumerable extension tests

---
+++ LittleHelpers/LittleHelpers.Tests/Data/TestList.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace LittleHelpers.Tests.Data
+{
+    public static class TestList
+    {
+        public static List<int> IntList = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        public static List<int> EmptyIntList = new List<int>();
+    }
+}
---
+++ LittleHelpers/LittleHelpers.Tests/ExtensionMethods/EnumerableExtensionsTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using LittleHelpers.ExtensionMethods;
+using LittleHelpers.Tests.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LittleHelpers.Tests.ExtensionMethods
+{
+    [TestClass]
+    public class EnumerableExtensionsTests
+    {
+        [TestMethod]
+        public void EnumerableExtensions_PickRandom_ReturnsOneValue()
+        {
+            var pickRandomValue = TestList.IntList.PickRandom();
+
+            Assert.AreEqual(typeof(int), pickRandomValue.GetType());
+            Assert.IsTrue(TestList.IntList.Contains(pickRandomValue));
+        }
+
+        [TestMethod]
+        public void EnumerableExtensions_PickRandom_ReturnsProvidedCountValues()
+        {
+            var pickRandomValues = TestList.IntList.PickRandom(2).ToList();
+
+            Assert.AreEqual(2, pickRandomValues.Count);
+            Assert.IsTrue(TestList.IntList.Contains(pickRandomValues[0]));
+            Assert.IsTrue(TestList.IntList.Contains(pickRandomValues[1]));
+        }
+
+
+        [TestMethod]
+        public void EnumerableExtensions_ToObservableCollection_ReturnsAnObservableCollection()
+        {
+            var observableCollection = TestList.IntList.ToObservableCollection();
+
+            Assert.AreEqual(typeof(ObservableCollection<int>), observableCollection.GetType());
+        }
+
+        [TestMethod]
+        public void EnumerableExtensions_HasItems_ReturnsTrueOnNonEmptyList()
+        {
+            var hasItems = TestList.IntList.HasItems();
+
+            Assert.IsTrue(hasItems);
+        }
+
+
+        [TestMethod]
+        public void EnumerableExtensions_HasItems_ReturnsFalseOnEmptyList()
+        {
+            var hasItems = TestList.EmptyIntList.HasItems();
+
+            Assert.IsFalse(hasItems);
+        }
+    }
+}
