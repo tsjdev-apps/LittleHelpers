f24d56d578da58fae1f9f9f68764a6301d120b13
Sebastian Jensen <apps@tsjdev-apps.de>
2017-11-18 18:17:01 +0100

    - add exception extensions

---
+++ LittleHelpers/LittleHelpers/ExtensionMethods/ExceptionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace LittleHelpers.ExtensionMethods
+{
+    public static class ExceptionExtensions
+    {
+        /// <summary>
+        /// Logs an exception as debug output.
+        /// </summary>
+        /// <param name="exception">The exception.</param>
+        public static void LogDebug(this Exception exception)
+        {
+            Debug.WriteLine($"EXCEPTION: {exception}");
+        }
+    }
+}
