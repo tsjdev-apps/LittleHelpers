d078ed3613362fae5e0a60a28a1475ae312839a3
Sebastian Jensen <apps@tsjdev-apps.de>
2017-11-18 16:55:11 +0100

    - add object extensions

---
+++ LittleHelpers/LittleHelpers/ExtensionMethods/ObjectExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace LittleHelpers.ExtensionMethods
+{
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Determines whether the object is not null.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        public static bool IsNotNull(this object obj)
+        {
+            return obj != null;
+        }
+        
+        /// <summary>
+        /// Determines whether the object is null.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        public static bool IsNull(this object obj)
+        {
+            return obj == null;
+        }
+
+        /// <summary>
+        /// Returns every object as string.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        public static string AsString(this object obj)
+        {
+            return obj == null ? string.Empty : obj.ToString();
+        }
+    }
+}
